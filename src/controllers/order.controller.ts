5909605b1a8f7b17928a722fae334f13c5e0b1dc
Angel Landeros <angellanderos568@gmail.com>
2025-10-07 16:33:55 -0600

    Prueba

--- src/controllers/order.controller.ts
+++ src/controllers/order.controller.ts
@@ -56,8 +56,9 @@ createNewOrder: async (req: Request, res: Response) => {
     
     // Enviar TODO el objeto al script de Python 
     // await printTicket({ products: order.products,total: order.total, table: order.table });
-    
+  
     return res.status(200).json("Orden creada correctamente");
+    
   } catch (error) {
     console.error(error);
     return res.status(500).json("Error al crear la orden");
